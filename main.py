8a042bafb1cdb91ddb532c71e7fea7d36fd7ccc2
maxization <damian.opoka99@gmail.com>
2022-05-11 21:38:04 +0200

    first version of cma-es

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from cma_es import cma_es
+import numpy as np
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cma_es(np.array([10.0, 10.0]), 5, lambda x, y: -(x * y) ** 2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
