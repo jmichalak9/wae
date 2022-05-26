bfb280b1076e4a37d12a3ab55c538f853e00d246
maxization <damian.opoka99@gmail.com>
2022-05-26 22:50:09 +0200

    add chi distribution

--- cma_es.py
+++ cma_es.py
@@ -1,67 +1,70 @@
 import numpy as np
-import time
 
 
-def cma_es(y, sigma, fun):
-    p = 0
-    s = 0
-    N = y.size
-    initial_sigma = sigma
-    offspring_size = int(4 + 3 * np.floor(np.log(N)))
-    generation_length = int(10 + np.ceil(30 * N / offspring_size))
-    mu = int(np.floor(offspring_size / 2))  # Number of best individuals
-    w = np.full(N, 1.0 / mu)
-    mu_eff = 1 / np.sum(w ** 2)
-    cp = (mu_eff / N + 4) / (2 * mu_eff / N + N + 4)
-    cs = (mu_eff + 2) / (mu_eff + N + 5)
-    alpha_cov = 2  # Can be in (0,2>
-    c1 = alpha_cov / ((N + 1.3) ** 2 + mu_eff)
-    cw = np.minimum(1 - c1, alpha_cov * (mu_eff + 1 / mu_eff - 2) / ((N + 2) ** 2 + alpha_cov * mu_eff / 2))
-    C = np.identity(N)
-    best_values = []
+class CMAES:
 
-    while True:
-        evalues, evectors = np.linalg.eig(C)
-        M = evectors @ np.diag(np.sqrt(evalues)) @ np.linalg.inv(evectors)
+    @staticmethod
+    def calculate(y, sigma, fun):
+        p = 0
+        s = 0
+        N = y.size
+        initial_sigma = sigma
+        offspring_size = int(4 + 3 * np.floor(np.log(N)))
+        generation_length = int(10 + np.ceil(30 * N / offspring_size))
+        mu = int(np.floor(offspring_size / 2))  # Number of best individuals
+        w = np.full(N, 1.0 / mu)
+        mu_eff = 1 / np.sum(w ** 2)
+        cp = (mu_eff / N + 4) / (2 * mu_eff / N + N + 4)
+        cs = (mu_eff + 2) / (mu_eff + N + 5)
+        alpha_cov = 2  # Can be in (0,2>
+        c1 = alpha_cov / ((N + 1.3) ** 2 + mu_eff)
+        cw = np.minimum(1 - c1, alpha_cov * (mu_eff + 1 / mu_eff - 2) / ((N + 2) ** 2 + alpha_cov * mu_eff / 2))
+        covariance_matrix = np.identity(N)
+        best_values = []
 
-        solutions = []
-        for i in range(offspring_size):
-            z = np.random.randn(N)
-            d = M @ z
-            new_y = y + sigma * d
-            fitness = fun(*new_y)
-            solutions.append((fitness, new_y, z, d))
+        while True:
+            square_root = np.linalg.cholesky(covariance_matrix)
+            solutions = []
+            for i in range(offspring_size):
+                z = np.random.randn(N)
+                d = square_root @ z
+                new_y = y + sigma * d
+                fitness = fun(*new_y)
+                solutions.append((fitness, new_y, z, d))
 
-        solutions.sort(key=lambda x: x[0], reverse=True)
+            solutions.sort(key=lambda x: x[0], reverse=True)
 
-        fitness_values = [solution[0] for solution in solutions]
+            fitness_values = [solution[0] for solution in solutions]
 
-        # Remember 'generation_length' maximum and minimum values
-        if len(best_values) == generation_length * 2:
-            best_values.pop(0)
-            best_values.pop(0)
+            # Remember 'generation_length' maximum and minimum values
+            if len(best_values) == generation_length * 2:
+                best_values.pop(0)
+                best_values.pop(0)
 
-        best_values.append(np.amin(fitness_values))
-        best_values.append(np.amax(fitness_values))
+            best_values.append(np.amin(fitness_values))
+            best_values.append(np.amax(fitness_values))
 
-        zz = [solution[2] for solution in solutions[:mu]]
-        dd = [solution[3] for solution in solutions[:mu]]
+            zz = [solution[2] for solution in solutions[:mu]]
+            dd = [solution[3] for solution in solutions[:mu]]
 
-        y += sigma * np.sum(w * dd, axis=0)
-        s = (1 - cs) * s + np.sqrt(mu_eff * cs * (2 - cs)) * np.sum(w * zz, axis=0)
-        p = (1 - cp) * p + np.sqrt(mu_eff * cp * (2 - cp)) * np.sum(w * dd, axis=0)
+            y += sigma * np.sum(w * dd, axis=0)
+            s = (1 - cs) * s + np.sqrt(mu_eff * cs * (2 - cs)) * np.sum(w * zz, axis=0)
+            p = (1 - cp) * p + np.sqrt(mu_eff * cp * (2 - cp)) * np.sum(w * dd, axis=0)
 
-        C = (1 - c1 - cw) * C + c1 * p * np.transpose(p) + cw * np.sum(w * dd * np.transpose(dd))
+            covariance_matrix = (1 - c1 - cw) * covariance_matrix + c1 * p * np.transpose(p) + cw * np.sum(w * dd * np.transpose(dd))
 
-        D = np.sqrt(N)
-        sigma = sigma * np.exp((1 / 2 * D) * ((np.linalg.norm(s) ** 2) / N - 1))  # Change to Chi distribution
+            # mean of chi distribution with N degrees of freedom using Stirling's approximation
+            chi_n = np.sqrt(N) * (1 - 1 / (4 * N) + 1 / (21 * N**2))
+            damping = 1 + cs + 2 * np.maximum(0, np.sqrt((mu_eff - 1) / (N + 1)) - 1)
 
-        print([solution[0] for solution in solutions[:mu]])
+            sigma *= np.exp(cs / damping * (np.linalg.norm(s) / chi_n - 1))
 
-        # First stop condition
-        if sigma < initial_sigma * 10 ** -12:
-            break
+            print([solution[0] for solution in solutions[:mu]])
 
-        # Second stop condition
-        if np.amax(best_values) - np.amin(best_values) < 10 ** -12:
-            break
+            # First stop condition
+            if sigma < initial_sigma * 10 ** -12:
+                return np.amin(best_values)
+
+            # Second stop condition
+            if np.amax(best_values) - np.amin(best_values) < 10 ** -12:
+                return np.amin(best_values)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-from cma_es import cma_es
-from ma_es import ma_es
+from cma_es import CMAES
 import numpy as np
+import math
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ma_es(np.array([10.0, 10.0]), 5, lambda x, y: -(x * y) ** 2)
+    cmaes = CMAES()
+    cmaes.calculate(np.array([10.0, 10.0, 10.0]), 5, lambda x, y, z: -(x * y * z) ** 2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
